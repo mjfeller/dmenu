8ce94ff0ee52650003e8b297fd6a349dd106ed48
Mark Feller <mark@mfeller.io>
2022-12-02 14:50:27 -0700

    personalized

--- config.def.h
+++ config.def.h
@@ -6,19 +6,19 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Fira Mono:size=10"
+	"Comic Mono:pixelsize=14:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm]   = { "#ebdbb2", "#161819" },
-	[SchemeSel]    = { "#98971a", "#1d2021" },
+	/*                 fg          bg       */
+	[SchemeNorm]   = { "#ffffff", "#000000" },
+	[SchemeSel]    = { "#44bc44", "#100f10" },
 	[SchemeOut]    = { "#000000", "#161819" },
 	[SchemeBorder] = { "#000000", "#aaaaaa" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 25;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 20;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
@@ -27,4 +27,4 @@ static unsigned int lineheight = 25;         /* -h option; minimum height of a m
 static const char worddelimiters[] = " ";
 
 /* Size of the window border */
-static unsigned int border_width = 2;
+static unsigned int border_width = 1;
